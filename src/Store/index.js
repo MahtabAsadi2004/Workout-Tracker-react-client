592c5190632c82b53a0be8ae02ed3da14043b8f9
MahtabAsadi <asadimahtaab@gmail.com>
2026-02-06 17:34:05 -0800

    add auth slice and store configuration

---
+++ src/Store/index.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authSliceReducer from "./Slice/AuthSlice";
+const store = configureStore({
+  reducer: { auth: authSliceReducer },
+});
+
+export default store;
